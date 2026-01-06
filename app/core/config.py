70efc38c2ed35586c74a1f730ae2a7ed0bd4e216
José Álvarez <josemejia61976751@gmail.com>
2026-01-06 12:59:27 -0600

    feat: initial database, docker and files configuration added, the API is sucessfully created

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings): 
+    PROJECT_NAME: str = "Vor API"
+    API_V1_STR: str = "/api/v1"
+
+    #Default configurations to work with docker containers
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@db:5432/vor_db"
+
+    class Config:
+        case_sensitive = True
+
+settings = Settings()
